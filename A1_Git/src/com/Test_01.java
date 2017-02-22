0badc0dc3909088564bee295730ecb194a141437
juan <juan@juan>
2017-02-22 16:25:05 +0100

    prueba

---
+++ A1_Git/src/com/Test_01.java
@@ -0,0 +1,15 @@
+package com;
+
+public abstract class Test_01 {
+
+	public Test_01() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hola32");
+		System.out.println("hola32");
+
+	}
+
+}
